6e67fffb535133f27fbbe27c152f527fb820af0f
Nick Dunn <nick.dunn@airlock.com>
2011-02-05 16:45:26 +0000

    Updated for full 2.2 compatibility

--- lib/class.cacheabledatasource.php
+++ lib/class.cacheabledatasource.php
@@ -150,6 +150,11 @@ Class CacheableDatasource extends Datasource {
 	            include(TOOLKIT . '/data-sources/datasource.section.php');
 	        }
 		}
+		catch(FrontendPageNotFoundException $e){
+			// Work around. This ensures the 404 page is displayed and
+			// is not picked up by the default catch() statement below
+			FrontendPageNotFoundExceptionHandler::render($e);
+		}
 		catch(Exception $e){
 			$result->appendChild(new XMLElement('error', $e->getMessage()));
 			return $result;
