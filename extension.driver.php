7f516159cc0c8cafa986df9cc5aca88d9050e442
Nick Dunn <nick@nick-dunn.co.uk>
2011-10-03 13:52:47 +0200

    Add current DS's cache value to page context for JS to pick up and display as input

--- extension.driver.php
+++ extension.driver.php
@@ -40,6 +40,11 @@
 					'delegate'	=> 'DatasourcePreEdit',
 					'callback'	=> 'dataSourceSave'
 				),
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'initaliseAdminPageHead'
+				)
 			);
 		}
 		
@@ -199,6 +204,38 @@
 			
 			$context['contents'] = $contents;
 		}
+		
+		public function initaliseAdminPageHead($context) {
+			$callback = (object)$context['parent']->getPageCallback();
+			if(isset($callback) && $callback->driver == 'blueprintsdatasources') {
+				
+				$cache = 0;
+				if($callback->context[0] == 'edit') {
+					$ds = $callback->context[1];
+					$dsm = new DatasourceManager(Symphony::Engine());
+					$datasource = $dsm->create($ds, NULL, false);
+					$cache = $datasource->dsParamCACHE;
+				}
+				
+				Administration::instance()->Page->addElementToHead(
+					new XMLElement(
+						'script',
+						"Symphony.Context.add('cacheabledatasource', " . json_encode(array(
+							'cache' => (isset($_POST['fields']['cache']) ? $_POST['fields']['cache'] : $cache)
+						)) . ");",
+						array('type' => 'text/javascript')
+					), time()
+				);
+				
+				Administration::instance()->Page->addScriptToHead(
+					URL . '/extensions/cacheabledatasource/assets/cacheabledatasource.blueprintsdatasources.js',
+					time()
+				);
+				
+			}
+			
+		}
+		
 	}
 
 ?>
